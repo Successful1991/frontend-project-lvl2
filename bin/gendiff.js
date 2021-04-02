2cac9995b67160dcadb38859951a93d3282b7c13
artem <successful1991@gmail.com>
2021-04-02 22:12:23 +0300

    fix gendiff bin

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,14 +3,12 @@
 import program from 'commander';
 import genDiff from '../src/genDiff.js';
 
-export default function gendiff() {
-  program
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((file1, file2, option) => {
-      console.log(genDiff(file1, file2, option.format));
-    });
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((file1, file2, option) => {
+    console.log(genDiff(file1, file2, option.format));
+  });
 
-  program.parse(process.argv);
-}
+program.parse(process.argv);
